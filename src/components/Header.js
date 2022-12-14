d5e39e98b393f7f35459c68462bd7c6260fdff88
SifatNiloy <sifatniloy18@gmail.com>
2022-12-14 20:01:54 +0600

    button added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
+import { Button } from '@mui/material';
 import React from 'react';
 
 const Header = () => {
     return (
         <div>
             <h2>This is Header</h2>
+            <Button variant="contained" color="success">Hello World</Button>
         </div>
     );
 };
