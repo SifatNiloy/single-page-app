de5622228ded036aeeae125743cd6dcdd9b5db93
SifatNiloy <sifatniloy18@gmail.com>
2022-12-19 13:16:41 +0600

    react router dom installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import { Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <Header></Header>
+      <Routes>
+
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
